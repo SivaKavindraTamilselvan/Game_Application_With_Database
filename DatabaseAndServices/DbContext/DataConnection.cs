9d0aacacc80c676c6efe4fdd56e8f0f10f1c6dd8
SivaKavindraTamilselvan <sivakavindra@gmail.com>
2026-05-12 22:44:21 +0530

    comment lines added

--- DatabaseAndServices/DbContext/DataConnection.cs
+++ DatabaseAndServices/DbContext/DataConnection.cs
@@ -3,7 +3,8 @@ namespace WordGame.DBContext;
 //usage of Npgsql Connection to connect to the database
 public class DataConnection
 {
-    private readonly string db_connection_string = "Host=localhost;Port=5432;Database=WordGame;Username=sivakavindra;Password=SivaKavindra@280804";
+    // need to enter correct usernme and password
+    private readonly string db_connection_string = "Host=localhost;Port=5432;Database=WordGame;Username=username;Password=password";
     public NpgsqlConnection GetConnection()
     {
         return new NpgsqlConnection(db_connection_string);
